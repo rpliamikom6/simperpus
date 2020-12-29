bdbadd50e30074e9f762e12c53454483ef81bdff
Siti Wijayanti <siti.wijayanti@students.amikom.ac.id>
2020-12-29 14:17:12 +0700

    Detail katalog

--- application/controllers/Katalog.php
+++ application/controllers/Katalog.php
@@ -14,14 +14,17 @@ class Katalog extends CI_Controller {
 		$data['books']=$this->Katalog_model->get_data();
 		$this->load->view('layout/wrapper', $data);
     }
-    
-    public function buku($id=Null){
-        if(!isset($id)) show_404();
-	}
 	
 	public function detail($id=NULL){
 		if(!isset($id)) show_404();
 		$data=array('title'=>'Judul halaman','content'=>'detail');
-		$this->load->view('layout/wrapper',$data);
+		$data['katalog']=$this->Katalog_model->get_data($id);
+		if($data['katalog']->num_rows()){
+			$data['detail']=$this->Katalog_model->get_data_detail_buku($id);
+			$this->load->view('layout/wrapper',$data);
+		}
+		else{
+			echo show_404();
+		}
 	}
 }
--- application/models/Katalog_model.php
+++ application/models/Katalog_model.php
@@ -2,8 +2,17 @@
     class Katalog_model extends CI_Model {
         var $table='buku';
 
-        public function get_data(){
+        public function get_data($id=NULL){
+            if(isset($id)){
+                $this->db->where('id_buku',$id);
+                $this->db->limit(1);
+            }
             return $this->db->get($this->table);
+        }
+
+        public function get_data_detail_buku($id_buku){
+            $this->db->where('id_buku',$id_buku);
+            return $this->db->get('detail_buku');
         }
 
     }
