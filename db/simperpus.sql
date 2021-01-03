301bf6f99a169b1bb154f1754ff14aea508e97e6
Siti Wijayanti <siti.wijayanti@students.amikom.ac.id>
2021-01-03 17:40:35 +0700

    db

---
+++ db/simperpus.sql
@@ -0,0 +1,372 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 03 Jan 2021 pada 11.39
+-- Versi server: 10.4.14-MariaDB
+-- Versi PHP: 7.3.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `simperpus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(6) NOT NULL,
+  `id_master_person` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(6) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `alamat` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `anggota`
+--
+
+CREATE TABLE `anggota` (
+  `id_anggota` int(11) NOT NULL,
+  `id_master_person` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` int(6) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `judul` varchar(30) NOT NULL,
+  `gambar` varchar(100) NOT NULL,
+  `id_penerbit` int(6) NOT NULL,
+  `tahun` year(4) NOT NULL,
+  `pengarang` varchar(20) NOT NULL,
+  `sinopsis` longtext NOT NULL,
+  `penerbit` varchar(100) NOT NULL,
+  `kota_terbit` varchar(100) NOT NULL,
+  `isbn` decimal(13,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `buku`
+--
+
+INSERT INTO `buku` (`id_buku`, `id_kategori`, `judul`, `gambar`, `id_penerbit`, `tahun`, `pengarang`, `sinopsis`, `penerbit`, `kota_terbit`, `isbn`) VALUES
+(12, 1, 'PHP Object Oriented Programmin', '', 3, 0000, '', 'weifewinbfiwebf', '', '', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_buku`
+--
+
+CREATE TABLE `detail_buku` (
+  `id_buku` int(11) NOT NULL,
+  `detail` varchar(100) NOT NULL,
+  `value` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `detail_buku`
+--
+
+INSERT INTO `detail_buku` (`id_buku`, `detail`, `value`) VALUES
+(12, '32rj23fn', 'jdwcnjkwenf');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `karyawan`
+--
+
+CREATE TABLE `karyawan` (
+  `nik` int(16) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `id_anggota` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `url_icon` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id`, `nama`, `url_icon`, `created_at`) VALUES
+(1, 'Jurnal', 'journal.png', '2020-12-24 05:39:44'),
+(2, 'Novel', '', '2020-12-24 05:40:03'),
+(3, 'Multimedia', '', '2020-12-24 05:40:22'),
+(4, 'Sejar', '', '2021-01-02 12:53:31');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id_mahasiswa` int(6) DEFAULT NULL,
+  `nim` int(10) NOT NULL,
+  `id_anggota` int(6) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `email` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `master_person`
+--
+
+CREATE TABLE `master_person` (
+  `id` int(11) NOT NULL,
+  `identity` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` longtext NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `role` enum('publik','mhs','karyawan') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `master_person`
+--
+
+INSERT INTO `master_person` (`id`, `identity`, `nama`, `alamat`, `email`, `role`, `created_at`, `updated_at`) VALUES
+(1, '17.11.1337', 'Edwin Rahmad Toha', 'Jalan Patehan Lor 22A', 'edwin.toha@students.amikom.ac.id', 'mhs', '2021-01-03 10:12:30', '2021-01-03 10:33:05'),
+(3, '', 'Siti Wijayanti', 'Ngentak', 'siti.wijayanti@students.amikom.ac.id', 'mhs', '2021-01-03 10:37:03', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id_peminjaman` int(6) NOT NULL,
+  `tgl_pinjam` date NOT NULL,
+  `tgl_habis_pinjam` date NOT NULL,
+  `tgl_kembali` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penerbit`
+--
+
+CREATE TABLE `penerbit` (
+  `id_penerbit` int(6) NOT NULL,
+  `nama_penerbit` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `penerbit`
+--
+
+INSERT INTO `penerbit` (`id_penerbit`, `nama_penerbit`) VALUES
+(3, 'Kemendikbud');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengarang`
+--
+
+CREATE TABLE `pengarang` (
+  `id_pengarang` int(11) NOT NULL,
+  `nama_pengarang` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(6) NOT NULL,
+  `id_buku` int(6) NOT NULL,
+  `id_peminjaman` int(6) NOT NULL,
+  `id_anggota` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `id_master_person` (`id_master_person`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indeks untuk tabel `anggota`
+--
+ALTER TABLE `anggota`
+  ADD PRIMARY KEY (`id_anggota`);
+
+--
+-- Indeks untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`),
+  ADD KEY `id_penerbit` (`id_penerbit`),
+  ADD KEY `id_kategori` (`id_kategori`);
+
+--
+-- Indeks untuk tabel `detail_buku`
+--
+ALTER TABLE `detail_buku`
+  ADD UNIQUE KEY `id_buku` (`id_buku`,`detail`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `master_person`
+--
+ALTER TABLE `master_person`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `penerbit`
+--
+ALTER TABLE `penerbit`
+  ADD PRIMARY KEY (`id_penerbit`);
+
+--
+-- Indeks untuk tabel `pengarang`
+--
+ALTER TABLE `pengarang`
+  ADD PRIMARY KEY (`id_pengarang`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `anggota`
+--
+ALTER TABLE `anggota`
+  MODIFY `id_anggota` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id_buku` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `master_person`
+--
+ALTER TABLE `master_person`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `penerbit`
+--
+ALTER TABLE `penerbit`
+  MODIFY `id_penerbit` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengarang`
+--
+ALTER TABLE `pengarang`
+  MODIFY `id_pengarang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`id_master_person`) REFERENCES `master_person` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  ADD CONSTRAINT `buku_ibfk_1` FOREIGN KEY (`id_penerbit`) REFERENCES `penerbit` (`id_penerbit`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `buku_ibfk_2` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `detail_buku`
+--
+ALTER TABLE `detail_buku`
+  ADD CONSTRAINT `detail_buku_ibfk_1` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
