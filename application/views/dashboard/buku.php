dc807eb854e9d871c87666eddd55b313699aa640
Siti Wijayanti <siti.wijayanti@students.amikom.ac.id>
2021-01-03 09:35:59 +0700

    sidebar module dashboard/buku

--- application/views/dashboard/buku.php
+++ application/views/dashboard/buku.php
@@ -7,20 +7,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-3">
-                <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= base_url('dashboard/buku');?>">Buku</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
-                    </li>
-                </ul>
+                <?php require_once('layout/menu.php');?>
             </div>
             <div class="col-md-9">
                 <div class="table-responsive">
