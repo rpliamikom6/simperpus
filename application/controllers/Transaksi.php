86df17a88ef26e99937a0526a700480c39658213
Siti Wijayannti <siti.wijayanti@students.amikom.ac.id>
2021-01-07 12:50:53 +0700

    Cart

--- application/controllers/Transaksi.php
+++ application/controllers/Transaksi.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Kategori extends CI_Controller {
+class Transaksi extends CI_Controller {
 
 	public function __construct(){
 		parent::__construct();
@@ -13,13 +13,22 @@ class Kategori extends CI_Controller {
 		$data=array('content'=>'kategori');
 		$data['kategori']=$this->Transaksi_model->get_data();
 		$this->load->view('layout/wrapper', $data);
-    }
+	}
     
     public function add_cart($id_buku,$redirect=NULL){
+		$i=0;
         if(!is_null(get_cookie('cart'))){
-            $cart=get_cookie('cart');
-        }
-
-        set_cookie('cart','cookie_value','86400');
+			$cart=get_cookie('cart');
+			$i=sizeof($cart);
+			delete_cookie('cart');
+		}
+		$cart[$i]=$id_buku;
+		set_cookie('cart','cookie_value','86400');
+		if(isset($redirect)){
+			redirect($redirect);
+		}
+		else{
+			redirect(base_url('katalog'));
+		}
     }
 }
--- application/views/katalog.php
+++ application/views/katalog.php
@@ -15,7 +15,7 @@
                         <h5 class="card-title"><?= $row->judul;?></h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
                             content.</p>
-                        <a href="#" class="btn btn-primary">Pinjam</a>
+                        <a href="<?= base_url('transaksi/add_cart/'.$row->id_buku);?>" class="btn btn-primary">Pinjam</a>
                     </div>
                 </div>
             </div>
